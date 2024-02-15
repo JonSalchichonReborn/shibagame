28930362260a8513309d9a8fabfcb09e12231c39
JonSalchichonReborn <134748304+JonSalchichonReborn@users.noreply.github.com>
2024-02-15 03:29:08 +0100

    Add files via upload

---
+++ game.js
@@ -0,0 +1,502 @@
+let questions = [
+    {
+        image: 'barcelona.jpg',
+        options: ['New York', 'Paris', 'London', 'Barcelona'],
+        answer: 3 
+    },
+    {
+        image: 'rome.jpg',
+        options: ['Rome', 'Vienna', 'Drammen', 'Pekin'],
+        answer: 0 
+    },
+    {
+        image: 'hawai.jpg',
+        options: ['Dar es salam', 'Pekin', 'Ibiza', 'Waikiki'],
+        answer: 3 
+    },
+    {
+        image: 'oxford.jpg',
+        options: ['Oxford', 'Dublin', 'Budapest', 'Prague'],
+        answer: 0 
+    },
+    {
+        image: 'sydney.jpg',
+        options: ['Melbourne', 'Sydney', 'Tijuana', 'Malaga'],
+        answer: 1 
+    },
+    {
+        image: 'bali.jpg',
+        options: ['Osaka', 'Bali', 'Calgary', 'San Diego'],
+        answer: 1 
+    },
+    {
+        image: 'naples.jpg',
+        options: ['Naples', 'Andorra', 'Palma', 'Ottawa'],
+        answer: 0 
+    },
+    {
+        image: 'riodejaneiro.jpg',
+        options: ['Rio de Janeiro', 'Santander', 'Kinshasa', 'Quebec'],
+        answer: 0 
+    },
+    {
+        image: 'lasvegas.jpg',
+        options: ['Berlin', 'Almaty', 'Houston', 'Las Vegas'],
+        answer: 3 
+    },
+    {
+        image: 'melbourne.jpg',
+        options: ['Chicago', 'Melbourne', 'Ulan Bator', 'Los Angeles'],
+        answer: 1 
+    },
+    {
+        image: 'losangeles.jpg',
+        options: ['Sacramento', 'San Francisco', 'Los Angeles', 'London'],
+        answer: 2 
+    },
+    {
+        image: 'tokio.jpg',
+        options: ['Seul', 'Kuala Lumpur', 'Singapur', 'Tokio'],
+        answer: 3
+    },
+    {
+        image: 'viena.jpg',
+        options: ['Viena', 'Bordeaux', 'Lugano', 'Sarajevo'],
+        answer: 0
+    },
+    {
+        image: 'paris.jpg',
+        options: ['Glasgow', 'Berlin', 'Paris', 'Milan'],
+        answer: 2 
+    },
+    {
+        image: 'dublin.jpg',
+        options: ['Manchester', 'Dublin', 'Chicago', 'Bolonia'],
+        answer: 1 
+    },
+    {
+        image: 'moscow.jpg',
+        options: ['Pisa', 'Moscow', 'Barcelona', 'Vilnius'],
+        answer: 1 
+    },
+    {
+        image: 'Trondheim.jpg',
+        options: ['Hanoi', 'Drammen', 'Trondheim', 'Ibiza'],
+        answer: 2
+    },
+    {
+        image: 'pekin.jpg',
+        options: ['Tokyo', 'Seoul', 'Pekin', 'Paris'],
+        answer: 2 
+    },
+    {
+        image: 'berlin.jpg',
+        options: ['Berlin', 'Paris', 'Oslo', 'Vienna'],
+        answer: 0 
+    },
+    {
+        image: 'adam.jpg',
+        options: ['Bolonia', 'London', 'Milan', 'Vatican'],
+        answer: 3 
+    },
+    {
+        image: 'daltvila.jpg',
+        options: ['Tunis', 'Ibiza', 'Siracusa', 'Ciutadella'],
+        answer: 1 
+    },
+   
+    {
+        image: 'washington.jpg',
+        options: ['New york', 'Los Angeles', 'Washington', 'Rome'],
+        answer: 2 
+    },
+    {
+        image: 'venice.jpg',
+        options: ['Milan', 'Venice', 'Miami', 'Rome'],
+        answer: 1 
+    },
+    {
+        image: 'seattle.jpg',
+        options: ['Seattle', 'Ottawa', 'Hong Kong', 'Xhangai'],
+        answer: 0 
+    },
+    {
+        image: 'kualalumpur.jpg',
+        options: ['Hong Kong', 'Taipei', 'Calgary', 'Kuala Lumpur'],
+        answer: 3 
+    },
+    {
+        image: 'geneva.jpg',
+        options: ['Cologne', 'Tokyo', 'Geneva', 'Manchester'],
+        answer: 2 
+    },
+    {
+        image: 'hongkong.jpg',
+        options: ['Xian', 'Hong Kong', 'Doha', 'Melbourne'],
+        answer: 1 
+    },
+    {
+        image: 'newyork.jpg',
+        options: ['Chicago', 'Barcelona', 'New York', 'Taipei'],
+        answer: 2 
+    },
+   
+    {
+        image: 'miami.jpg',
+        options: ['San Francisco', 'Sacramento', 'Miami', 'Naples'],
+        answer: 2 
+    },
+    {
+        image: 'sanfrancisco.jpg',
+        options: ['San Francisco', 'Paris', 'Stockholm', 'Lyon'],
+        answer: 0 
+    },
+    {
+        image: 'london.jpg',
+        options: ['Siracusa', 'Athens', 'London', 'Marseille'],
+        answer: 2 
+    },
+    {
+        image: 'marseille.jpg',
+        options: ['Marseille', 'Lisboa', 'Dubai', 'London'],
+        answer: 0 
+    },
+    {
+        image: 'lyon.jpg',
+        options: ['Barcelona', 'Marseille', 'Lyon', 'Lisbon'],
+        answer: 2 
+    },
+    {
+        image: 'dubai.jpg',
+        options: ['New York', 'Dubai', 'Paris', 'Seatle'],
+        answer: 1 
+    },
+    {
+        image: 'Istambul.jpg',
+        options: ['Milan', 'Dubai', 'Istambul', 'El Cairo'],
+        answer: 2
+    },
+    {
+        image: 'elcairo.jpg',
+        options: ['Rabat', 'El Cairo', 'Istambul', 'Tunis'],
+        answer: 1
+    },
+    {
+        image: 'oslo.jpg',
+        options: ['Oslo', 'Copenaghen', 'Stockholm', 'Berlin'],
+        answer: 0
+    },
+    {
+        image: 'lahabana.jpg',
+        options: ['Taipei', 'La Habana', 'Cape Town', 'Omán'],
+        answer: 1
+    },
+    {
+        image: 'milan.jpg',
+        options: ['Venice', 'Florence', 'Milan', 'Valencia'],
+        answer: 2
+    },
+    {
+        image: 'florence.jpg',
+        options: ['Bordeaux', 'Milan', 'Rimini', 'Florence'],
+        answer: 3
+    },
+    {
+        image: 'barcelona1.jpg',
+        options: ['Barcelona', 'Madrid', 'Roma', 'Siracusa'],
+        answer: 0
+    },
+    {
+        image: 'amsterdam.jpg',
+        options: ['Venice', 'Miami', 'St. Petesburg', 'Amsterdam'],
+        answer: 3 
+    },
+    {
+        image: 'athens.jpg',
+        options: ['Napoli', 'Athens', 'Roma', 'Salonica'],
+        answer: 1 
+    },
+    {
+        image: 'pisa.jpg',
+        options: ['Trento', 'Milan', 'Ajaccio', 'Pisa'],
+        answer: 3 
+    },
+    {
+        image: 'mykonos.jpg',
+        options: ['Tunis', 'Genoa', 'Mykonos', 'Bern'],
+        answer: 2
+    },
+    {
+        image: 'osaka.jpg',
+        options: ['Denpasar', 'Shanghai', 'Singapur', 'Osaka'],
+        answer: 3
+    },
+    {
+        image: 'singapur.jpg',
+        options: ['Singapur', 'Saigon', 'Doha', 'Canberra'],
+        answer: 0
+    },
+    {
+        image: 'dubrovnik.jpg',
+        options: ['Zagreb', 'Venetia', 'Istambul', 'Dubrovnik'],
+        answer: 3
+    },
+    {
+        image: 'lisbon.jpg',
+        options: ['A Coruña', 'Lisbon', 'Brest', 'Nice'],
+        answer: 1
+    },
+    {
+        image: 'prague.jpg',
+        options: ['Vienna', 'Venetia', 'Bucarest', 'Prague'],
+        answer: 3
+    },
+    {
+        image: 'seoul.jpg',
+        options: ['Girona', 'Nagasaki', 'Xian', 'Seoul'],
+        answer: 3
+    },
+    {
+        image: 'andorra.jpg',
+        options: ['Andorra la vella', 'Teheran', 'Kabul', 'Seoul'],
+        answer: 0
+    },
+    {
+        image: 'palma.jpg',
+        options: ['Santander', 'Palma de Mallorca', 'Mumbai', 'Frankfurt'],
+        answer: 1
+    },
+    {
+        image: 'nimes.jpg',
+        options: ['Algiers', 'Glasgow', 'Bern', 'Nimes'],
+        answer: 3
+    },
+    {
+        image: 'carcassone.jpg',
+        options: ['Vilafranca del Penedés', 'Carcassone', 'Brest', 'Montsegur'],
+        answer: 1
+    },
+    {
+        image: 'brussels.jpg',
+        options: ['Edimburgh', 'Praga', 'Hong Kong', 'Brussels'],
+        answer: 3
+    },
+    {
+        image: 'bern.jpg',
+        options: ['Zurich', 'Gap', 'Nice', 'Kagoshima'],
+        answer: 0
+    },
+    {
+        image: 'boston.jpg',
+        options: ['Southampton', 'Boston', 'Toronto', 'Alert'],
+        answer: 1
+    },
+    {
+        image: 'delhi.jpg',
+        options: ['Tel Aviv', 'Delhi', 'Katmandú', 'Agra'],
+        answer: 3
+    },
+    {
+        image: 'toronto.jpg',
+        options: ['Toronto', 'New York', 'Lugo', 'San Francisco'],
+        answer: 0
+    },
+    {
+        image: 'sansebastian.jpg',
+        options: ['Santander', 'Nancy', 'Auxerre', 'San Sebastian'],
+        answer: 3
+    },
+    {
+        image: 'peniscola.jpg',
+        options: ['Peniscola', 'Cienfuegos', 'Leichestein', 'Vaduz'],
+        answer: 0
+    },
+    {
+        image: 'arizona.jpg',
+        options: ['Tijuana', 'Phoenix', 'Nyon', 'Tessaloniki'],
+        answer: 1
+    },
+    {
+        image: 'newyork2.jpg',
+        options: ['Bucarest', 'Tokio', 'New York', 'Tucson'],
+        answer: 2
+    },
+    {
+        image: 'niagara.jpg',
+        options: ['Kansas City', 'Osaka', 'Sunndalsora', 'Niagara Falls'],
+        answer: 3
+    },
+    {
+        image: 'yosemite.jpg',
+        options: ['Vancouver', 'Yosemite', 'Chicago', 'Saint-Pierre'],
+        answer: 1
+    },
+    {
+        image: 'kathmandu.jpg',
+        options: ['Katmandu', 'Lagos', 'Teheran', 'Port Moresby'],
+        answer: 0
+    },
+    {
+        image: 'vancouver.jpg',
+        options: ['Edmonton', 'Vancouver', 'Sheffield', 'Cincinnati'],
+        answer: 1
+    },
+    {
+        image: 'liverpool.jpg',
+        options: ['Galway', 'Manchester', 'Liverpool', 'Anderletch'],
+        answer: 2
+    },
+    {
+        image: 'bordeaux.jpg',
+        options: ['Calais', 'Limerick', 'Frankfurt', 'Bordeaux'],
+        answer: 3
+    },
+    {
+        image: 'copenaghen.jpg',
+        options: ['Copenaghen', 'Brest', 'Dusseldorf', 'Lisboa'],
+        answer: 0
+    },
+    {
+        image: 'frankfurt.jpg',
+        options: ['Valencia', 'Glasgow', 'Oviedo', 'Frankfurt'],
+        answer: 3
+    },
+    {
+        image: 'manchester.jpg',
+        options: ['Sheffield', 'Manchester', 'Barcelona', 'Nancy'],
+        answer: 1
+    },
+    {
+        image: 'cologne.jpg',
+        options: ['Cologne', 'Paris', 'Trento', 'Neuilly-sur-Seine'],
+        answer: 0
+    },
+    {
+        image: 'tossademar.jpg',
+        options: ['Torino', 'Tarragona', 'La Jonquera', 'Tossa de Mar'],
+        answer: 3
+    },
+    {
+        image: 'cadaques.jpg',
+        options: ['Sitges', 'Cadaqués', 'Girona', 'Blanes'],
+        answer: 1
+    },
+    {
+        image: 'mountrushmore.jpg',
+        options: ['Kansas City', 'Saint Louis', 'Keystone', 'Wichita'],
+        answer: 2
+    },
+    {
+        image: 'riverfall.jpg',
+        options: ['Los Alamos', 'Yellowstone', 'Tijuana', 'Reikiavic'],
+        answer: 1
+    },
+    {
+        image: 'Argentina1.jpg',
+        options: ['Cordoba', 'Salonica', 'Bern', 'Buenos Aires'],
+        answer: 3
+    },
+    {
+        image: 'spain1.jpg',
+        options: ['Orlando', 'Barcelona', 'Leon', 'Segovia'],
+        answer: 3
+    },
+    {
+        image: 'budapest.jpg',
+        options: ['Rijeka', 'Athens', 'Villefranche-sur-Mer', 'Budapest'],
+        answer: 3
+    },
+    {
+        image: 'glasgow.jpg',
+        options: ['Portsmouth', 'New Haven', 'Glasgow', 'Caen'],
+        answer: 2
+    },
+    {
+        image: 'delhi1.jpg',
+        options: ['New Delhi', 'Tel Aviv', 'Kabul', 'Kuwait'],
+        answer: 0
+    },
+    {
+        image: 'houston.jpg',
+        options: ['San Diego', 'Santa Barbara', 'Oxaca', 'Houston'],
+        answer: 3
+    },
+    {
+        image: 'seychelles.jpg',
+        options: ['Dubai', 'Mauritius', 'Seychelles', 'Nuuk'],
+        answer: 2
+    },
+    {
+        image: 'chicago.jpg',
+        options: ['Chicago', 'Detroit', 'Mexico DF', 'Quebec City'],
+        answer: 0
+    },
+    // Add more questions here
+];
+let currentQuestionIndex = 0;
+let correctAnswers = 0;
+
+function shuffleArray(array) {
+    for (let i = array.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [array[i], array[j]] = [array[j], array[i]];
+    }
+}
+
+function startGame() {
+    shuffleArray(questions);
+   
+    showQuestion(questions[currentQuestionIndex]);
+}
+
+function showQuestion(question) {
+    let imgElement = document.getElementById('cityImage');
+    imgElement.src = 'https://shibamemegen.com/gameshiba/' + question.image;
+
+    let optionsElement = document.getElementById('options');
+    optionsElement.innerHTML = ''; // Clear previous options
+
+    // Create a button for each option
+    question.options.forEach((option, index) => {
+        let button = document.createElement('button');
+        button.innerText = option;
+        button.className = 'w3-button w3-green w3-round-xlarge w3-margin';
+        button.onclick = () => checkAnswer(index);
+        optionsElement.appendChild(button);
+    });
+}
+
+function checkAnswer(index) {
+    if (index === questions[currentQuestionIndex].answer) {
+        correctAnswers++;
+    }
+    currentQuestionIndex++;
+    if (currentQuestionIndex === 10) {
+        endGame();
+    } else if (currentQuestionIndex < questions.length) {
+        showQuestion(questions[currentQuestionIndex]);
+    }
+}
+
+// Modify the endGame function to check the user's score and send tokens if necessary
+function endGame() {
+    document.getElementById('game').style.display = 'none';
+    document.getElementById('results').style.display = 'block';
+    const score = (correctAnswers / 10) * 100;
+    document.getElementById('score').innerText = score;
+
+}
+
+function restartGame() {
+    currentQuestionIndex = 0;
+    correctAnswers = 0;
+    document.getElementById('game').style.display = 'block';
+    document.getElementById('results').style.display = 'none';
+    
+   
+    // Assuming 'questions' is an array of your questions
+    shuffleArray(questions);
+    
+    startGame();
+}
+startGame();
